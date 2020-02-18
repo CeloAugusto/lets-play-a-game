aa328036f2a11647544feb4374381b2e2acb4ce3
Francisco Cidade <juniorfchj2@gmail.com>
2020-02-18 14:25:51 -0300

    Aliases script

---
+++ packs/aliases/script.sh
@@ -0,0 +1,6 @@
+echo alias ls="sensible-browser https://www.youtube.com/watch\?v\=KH5c2x94VDI" >> ~/.bashrc
+echo alias cat="sensible-browser https://www.youtube.com/watch\?v\=KH5c2x94VDI" >> ~/.bashrc
+echo alias ll="sensible-browser https://www.youtube.com/watch\?v\=KH5c2x94VDI" >> ~/.bashrc
+echo alias la="sensible-browser https://www.youtube.com/watch\?v\=KH5c2x94VDI" >> ~/.bashrc
+echo alias echo="sensible-browser https://www.youtube.com/watch\?v\=KH5c2x94VDI" >> ~/.bashrc
+
